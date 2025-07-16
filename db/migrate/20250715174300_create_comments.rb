71a7137e9bf238c4d52173528cc5785f43384401
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-07-16 10:21:20 -0500

    Update comments table to have foreign key defined explicitly

--- db/migrate/20250715174300_create_comments.rb
+++ db/migrate/20250715174300_create_comments.rb
@@ -1,11 +1,16 @@
 class CreateComments < ActiveRecord::Migration[8.0]
   def change
     create_table :comments do |t|
+      t.integer :article_id
       t.string :commenter
       t.text :body
-      t.references :article, null: false, foreign_key: true
 
       t.timestamps
     end
+
+    add_foreign_key :comments,
+                    :articles,
+                    column: :article_id
+    add_index :comments, :article_id
   end
 end
